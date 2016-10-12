72a3f7e464495b37c600961fc92f5b29001edf48
Jordan Fearnley <fearnlj01@gmail.com>
2016-10-12 23:47:46 +0100

    Update NadekoInstall.bat
    I kinda added something extra so it needs testing again ¯\\\_(ツ)_/¯

--- NadekoInstall.bat
+++ NadekoInstall.bat
@@ -7,17 +7,17 @@ RMDIR %rootdir%\NadekoInstall_Temp /S /Q
 MKDIR NadekoInstall_Temp
 CD NadekoInstall_Temp
 git clone -b 1.0 --recursive https://github.com/Kwoth/NadekoBot.git
-dotnet restore
+dotnet restore > log:nul
 CD NadekoBot\src\NadekoBot\
 dotnet build --configuration Release
 IF EXIST %rootdir%\NadekoBot\ (
 ROBOCOPY %rootdir%\NadekoBot\ %rootdir%\NadekoBot_Old\ /E > log:nul
 ECHO Old files backed up to NadekoBot_Old
-RMDIR %rootdir%\NadekoBot /S /Q
+CMD /C RMDIR %rootdir%\NadekoBot /S /Q
 ROBOCOPY %rootdir%\NadekoInstall_Temp %rootdir%\ /E /MOVE > log:nul
-RMDIR %rootdir%\NadekoInstall_Temp /S /Q
+CMD /C RMDIR %rootdir%\NadekoInstall_Temp /S /Q
 COPY %rootdir%\NadekoBot_Old\src\NadekoBot\credentials.json %rootdir%\NadekoBot\src\NadekoBot\credentials.json
-COPY %rootdir%\NadekoBot_Old\src\NadekoBot\bin\Release\netcoreapp1.0\data\NadekoBot.db %rootdir%\NadekoBot\src\NadekoBot\bin\Release\netcoreapp1.0\data\NadekoBot.db
+CMD /C COPY %rootdir%\NadekoBot_Old\src\NadekoBot\bin\Release\netcoreapp1.0\data\NadekoBot.db %rootdir%\NadekoBot\src\NadekoBot\bin\Release\netcoreapp1.0\data\NadekoBot.db
 ) ELSE (
 ROBOCOPY %rootdir%\NadekoInstall_Temp %rootdir%\ /E /MOVE > log:nul
 )
