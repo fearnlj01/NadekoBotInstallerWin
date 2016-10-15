cc595df5bbf746e53444d02ecbe713e6e6d431dc
Jordan Fearnley <fearnlj01@gmail.com>
2016-10-15 04:52:34 +0100

    Suitable as an Installer now!
    It mostly supresses all the actual output in favour of simple, one line output :)
    It works everytime for me at least, but more testing is always good

--- NadekoInstall.bat
+++ NadekoInstall.bat
@@ -1,12 +1,12 @@
 @ECHO off
 @TITLE Downloading NadekoBot, please wait
-SET rootdir=%CD%
-SET nadeko=%CD%\NadekoInstall_Temp\NadekoBot\
+SET rootdir=%cd%
+SET nadeko=%cd%\NadekoInstall_Temp\NadekoBot\
 SET build1=%nadeko%discord.net\src\Discord.Net
 SET build2=%nadeko%discord.net\src\Discord.Net.Commands
 SET build3=%nadeko%src\NadekoBot
 IF EXIST %rootdir%\NadekoInstall_Temp\ (
-RMDIR %rootdir%\NadekoInstall_Temp /S /Q 2> nul
+RMDIR %rootdir%\NadekoInstall_Temp /S /Q
 )
 MKDIR NadekoInstall_Temp
 CD NadekoInstall_Temp
@@ -24,17 +24,21 @@ dotnet build --configuration Release >nul 2>&1
 IF EXIST %rootdir%\NadekoBot\ (
 @TITLE Backing up old files
 ROBOCOPY %rootdir%\NadekoBot\ %rootdir%\NadekoBot_Old\ /E >nul 2>&1
-ECHO $'Old files backed up to NadekoBot_Old\n'
+ECHO.
+ECHO Old files backed up to NadekoBot_Old
 CMD /C RMDIR %rootdir%\NadekoBot /S /Q >nul 2>&1
 ROBOCOPY %rootdir%\NadekoInstall_Temp %rootdir%\ /E /MOVE >nul 2>&1
 CMD /C RMDIR %rootdir%\NadekoInstall_Temp /S /Q >nul 2>&1
 COPY %rootdir%\NadekoBot_Old\src\NadekoBot\credentials.json %rootdir%\NadekoBot\src\NadekoBot\credentials.json >nul 2>&1
-ECHO $'credentials.json copied to new folder\n'
+ECHO.
+ECHO credentials.json copied to new folder
 CMD /C COPY %rootdir%\NadekoBot_Old\src\NadekoBot\bin\Release\netcoreapp1.0\data\NadekoBot.db %rootdir%\NadekoBot\src\NadekoBot\bin\Release\netcoreapp1.0\data\NadekoBot.db >nul 2>&1
-ECHO $'NadekoBot.db copied to new folder\n'
+ECHO.
+ECHO NadekoBot.db copied to new folder
 ) ELSE (
 ROBOCOPY %rootdir%\NadekoInstall_Temp %rootdir%\ /E /MOVE >nul 2>&1
 )
 @TITLE Installation complete!
-ECHO Installation complete
-PAUSE
+ECHO.
+ECHO Installation complete, press any key to close this window!
+PAUSE >nul 2>&1
