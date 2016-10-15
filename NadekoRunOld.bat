563ef63ee336fc3a54bedbf42e0481a19f7ee427
Jordan Fearnley <fearnlj01@gmail.com>
2016-10-15 05:20:38 +0100

    Update NadekoRunOld.bat

--- NadekoRunOld.bat
+++ NadekoRunOld.bat
@@ -1,7 +1,8 @@
-@echo off
-@title NadekoBot
+@ECHO off
+@TITLE NadekoBot
 CD NadekoBot_Old\src\NadekoBot
 dotnet run --configuration Release
 ECHO NadekoBot has been succesfully stopped, press any key to close this window.
+TITLE NadekoBot - Stopped
 CD %~dp0
 PAUSE >nul 2>&1
