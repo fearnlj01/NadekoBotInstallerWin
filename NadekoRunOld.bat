586886909a8e60f54540079a618f54c638dc888c
Jordan Fearnley <fearnlj01@gmail.com>
2016-10-22 05:30:26 +0100

    Delete NadekoRunOld.bat
    Goodbye sweet file, you were useful for testing while you existed

--- NadekoRunOld.bat
+++
@@ -1,8 +0,0 @@
-@ECHO off
-@TITLE NadekoBot
-CD NadekoBot_Old\src\NadekoBot
-dotnet run --configuration Release
-ECHO NadekoBot has been succesfully stopped, press any key to close this window.
-TITLE NadekoBot - Stopped
-CD %~dp0
-PAUSE >nul 2>&1
