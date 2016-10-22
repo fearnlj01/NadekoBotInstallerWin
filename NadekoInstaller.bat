9da0230991a51686024cb3330aa22711502dccbe
Jordan Fearnley <fearnlj01@gmail.com>
2016-10-22 06:51:49 +0100

    Updated for :sparkles:
    If it doesn't work I'm putting some of the blame on @Kwoth now :)

--- NadekoInstaller.bat
+++ NadekoInstaller.bat
@@ -1,32 +1,28 @@
 @ECHO off
 TITLE Downloading NadekoBot, please wait
-::Setting convenient to read variables
+::Setting convenient to read variables which don't delete the windows temp folder
 SET root=%~dp0
-SET build1=%root%NadekoInstall_Temp\NadekoBot\discord.net\src\Discord.Net\
-SET build2=%root%NadekoInstall_Temp\NadekoBot\discord.net\src\Discord.Net.Commands\
-SET build3=%root%NadekoInstall_Temp\NadekoBot\src\NadekoBot\
-::Deleting traces of last setup for the sake of clean folders
+CD /D %root%
+SET rootdir=%cd%
+SET build=%root%NadekoInstall_Temp\NadekoBot\src\NadekoBot\
+SET rootinstall=%root%NadekoInstall_Temp\NadekoBot\
+SET installtemp=%root%NadekoInstall_Temp\
+::Deleting traces of last setup for the sake of clean folders, if by some miracle it still exists
 IF EXIST "%root%NadekoInstall_Temp\" ( RMDIR "%root%NadekoInstall_Temp" /S /Q )
 ::Checks that both git and dotnet are installed
 dotnet --version >nul 2>&1 || GOTO :dotnet
 git --version >nul 2>&1 || GOTO :git
-::Creates the install directory to work in and get the current directory because spaces ruins everythin otherwise
-CD /D %root%
-SET rootdir=%cd%
+::Creates the install directory to work in and get the current directory because spaces ruins everything otherwise
 MKDIR NadekoInstall_Temp
-CD /D NadekoInstall_Temp
+CD /D %installtemp%
 ::Downloads the latest version of Nadeko
 git clone -b 1.0 --recursive -v https://github.com/Kwoth/NadekoBot.git >nul
 TITLE Installing NadekoBot, please wait
+ECHO Installing...
 ::Building Nadeko
-CD /D %build1%
+CD /D %rootinstall%
 dotnet restore >nul 2>&1
-dotnet build --configuration Release >nul 2>&1
-CD /D %build2%
-dotnet restore >nul 2>&1
-dotnet build --configuration Release >nul 2>&1
-CD /D %build3%
-dotnet restore >nul 2>&1
+CD /D %build%
 dotnet build --configuration Release >nul 2>&1
 ::Attempts to backup old files if they currently exist in the same folder as the batch file
 IF EXIST "%root%NadekoBot\" (GOTO :backupinstall)
@@ -40,14 +36,13 @@ IF EXIST "%root%NadekoBot\" (GOTO :backupinstall)
 	ROBOCOPY "%root%NadekoBot" "%root%NadekoBot_Old" /E /MOVE >nul 2>&1
 	ECHO.
 	ECHO Old files backed up to NadekoBot_Old
-	::Moves the setup Nadeko folder and deletes the remaining folders
+	::Moves the setup Nadeko folder
 	ROBOCOPY "%root%NadekoInstall_Temp" "%rootdir%" /E /MOVE >nul 2>&1
-	CMD /C RMDIR "%root%NadekoInstall_Temp" /S /Q >nul 2>&1
 	::Copies the credentials and database from the backed up data to the new folder
 	COPY "%root%NadekoBot_Old\src\NadekoBot\credentials.json" "%root%NadekoBot\src\NadekoBot\credentials.json" >nul 2>&1
 	ECHO.
 	ECHO credentials.json copied to new folder
-	CMD /C COPY "%root%NadekoBot_Old\src\NadekoBot\bin\Release\netcoreapp1.0\data\NadekoBot.db" "%root%NadekoBot\src\NadekoBot\bin\Release\netcoreapp1.0\data\NadekoBot.db" >nul 2>&1
+	COPY "%root%NadekoBot_Old\src\NadekoBot\bin\Release\netcoreapp1.0\data\NadekoBot.db" "%root%NadekoBot\src\NadekoBot\bin\Release\netcoreapp1.0\data\NadekoBot.db" >nul 2>&1
 	ECHO.
 	ECHO NadekoBot.db copied to new folder
 	GOTO :end
@@ -57,7 +52,7 @@ IF EXIST "%root%NadekoBot\" (GOTO :backupinstall)
 	ECHO dotnet not found, make sure it's been installed as per the guides instructions!
 	ECHO Press any key to exit.
 	PAUSE >nul 2>&1
-	CD /D "%root%" >nul 2>&1
+	CD /D "%root%"
 	GOTO :EOF
 :git
 	::Terminates the batch script if it can't run git --version
@@ -70,7 +65,8 @@ IF EXIST "%root%NadekoBot\" (GOTO :backupinstall)
 :end
 	::Normal execution of end of script
 	TITLE Installation complete!
+	CD /D "%root%" 
+	RMDIR /S /Q "%installtemp%" >nul 2>&1
 	ECHO.
 	ECHO Installation complete, press any key to close this window!
 	PAUSE >nul 2>&1
-	CD /D "%root%"
